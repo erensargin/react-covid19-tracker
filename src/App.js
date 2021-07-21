9be83bcf39ddef69c30664c6bd32ec9714b4580b
Eren Sargin <erensargin94@hotmail.com>
2021-07-21 14:29:04 +0300

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 
 import styles from './App.module.css'
-
+import { fetchData } from './api';
 
 function App() {
+
+  const data = fetchData();
+  console.log(data)
+
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1>App</h1>
       <Cards/>
       <Chart/>
