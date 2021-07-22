e33ebd0c028f3c9c071b58e1f8dbbd7bd9f4174c
Eren Sargin <erensargin94@hotmail.com>
2021-07-22 14:34:00 +0300

    Charts added, final version of the project published

--- src/App.js
+++ src/App.js
@@ -7,22 +7,39 @@ import styles from './App.module.css'
 import { fetchData } from './api';
 import { useEffect, useState } from 'react';
 
+import coronaImage from './images/image.png'
+
 
 function App() {
 
   const [data, setData] = useState([])
+  const [country, setCountry] = useState('')
 
-  useEffect( async () => {
-    setData(await fetchData()); 
+  useEffect( () => {
+    const fData = async () => {
+      setData(await fetchData());
+    }
+    fData();
   }, [])
 
+  const handleCountryChange = async (country) => {
+    const fData = async () => {
+      setData(await fetchData(country));
+      setCountry(country);
+    }
+    fData();
+
+    
+    
+  }
   
   return (
     <div className={styles.container}>
-      {data.confirmed ? <Cards data={data}/> : "no data"}
-      
-      <Chart/>
-      <CountryPicker/>
+      <img className={styles.image} src={coronaImage} alt="coronaImage" />
+      {data.confirmed ? 
+      <Cards data={data}/> : "no data"}
+      <Chart data={data} country={country} />
+      <CountryPicker handleCountryChange={handleCountryChange}/>
     </div>
   );
 }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
 
-function CountryPicker() {
-    return (
-        <div>
-            <h1>CountryPicker</h1>
-        </div>
-    )
+import styles from "./CountryPicker.module.css";
+import { fetchCountriesFunc } from "../../api";
+
+function CountryPicker({ handleCountryChange }) {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setFetchedCountries(await fetchCountriesFunc());
+    };
+
+    fetchCountries();
+  }, [setFetchedCountries]);
+
+  console.log(fetchedCountries);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="">Global</option>
+        {fetchedCountries.map((country, i) => (
+          <option value={country} key={i}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
 }
 
-export default CountryPicker
+export default CountryPicker;
