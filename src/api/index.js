fadc61cff08dbf447d185187d014ec22fb17653a
Eren Sargin <erensargin94@hotmail.com>
2021-07-22 10:20:50 +0300

    adding chart

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,33 @@
 import axios from "axios";
 
-const url = 'https://covid19.mathdro.id/api';
+const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
 
-    try{
+    const modifiedData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+    console.log(modifiedData);
 
-        const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+    return modifiedData;
+  } catch (error) {}
+};
 
-        const modifiedData = {
-            confirmed,
-            recovered,
-            deaths,
-            lastUpdate
-        }
-        console.log(modifiedData)
-
-        return modifiedData;
-
-    }
-    catch(error){
-
-    }
-
-
-}
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) =>({
+        confirmed: dailyData.confirmed.total,
+        deaths: dailyData.deaths.total,
+        date:dailyData.reportDate
+    }))
+    return modifiedData;
+  } catch (error) {}
+};
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
 
 function Chart() {
-    return (
-        <div>
-            <h1>Chart</h1>
-        </div>
-    )
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetch();
+  }, []);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
 }
 
-export default Chart
+export default Chart;
